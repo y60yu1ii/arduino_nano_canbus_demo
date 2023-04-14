6eddd1ceaa20bad1c37ff8921d7e028d16742fd7
yaoyu <yaoyuli@pqdusa.com>
2023-04-14 15:48:56 +0800

    1. fix canbus rate bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <CAN.h>
 #define LED_PIN 13
+#define CAN_125KBPS 250E3
+#define CAN_250KBPS 500E3
+#define CAN_500KBPS 1000E3
+// my arduino nano have bug so all bitrate should be doubled
+
 unsigned long previousMillis = 0;  // will store last time a CAN Message was send
 const int interval = 500;
 
@@ -15,19 +20,19 @@ void printf_begin(void) {
 }
 
 void setup() {
+    pinMode(LED_PIN, OUTPUT);
     Serial.begin(115200);
     printf_begin();
     Serial.println("Basic Demo - Arduino-Nano-CAN");
-    pinMode(LED_PIN, OUTPUT);
-    if (!CAN.begin(125E3)) {
+    if (!CAN.begin(CAN_125KBPS)) {
         Serial.println("Starting CAN failed!");
         while (1)
             ;
     }
     Serial.println("Starting CAN ok!");
-    // add filter to only receive the CAN bus ID's we care about
-    // CAN.filter(0x2);
-    // register the receive callback
+    // // add filter to only receive the CAN bus ID's we care about
+    // // CAN.filter(0x2);
+    // // register the receive callback
     CAN.onReceive(onReceive);
 }
 
@@ -50,19 +55,21 @@ void onReceive(int packetSize) {
 }
 
 int heartbeat = 0;
+unsigned long currentMillis;
 
 void loop() {
-    unsigned long currentMillis = millis();
+    currentMillis = millis();
 
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;
-        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        // printf(">>> ");
 
         heartbeat++;
         if (heartbeat > 15) {
             heartbeat = 0;
         }
-        CAN.beginPacket(0x1, 8);
+        // CAN.beginPacket(0x1, 8);
+        CAN.beginExtendedPacket(0x1, 8);
         CAN.write(heartbeat);
         CAN.write(0x02);
         CAN.write(0x03);
@@ -72,5 +79,29 @@ void loop() {
         CAN.write(0x07);
         CAN.write(0x08);
         CAN.endPacket();
+
+        CAN.beginExtendedPacket(0x3, 8);
+        CAN.write(heartbeat);
+        CAN.write(0x02);
+        CAN.write(0x03);
+        CAN.write(0x04);
+        CAN.write(0x05);
+        CAN.write(0x06);
+        CAN.write(0x07);
+        CAN.write(0x08);
+        CAN.endPacket();
+
+        CAN.beginExtendedPacket(0x4, 8);
+        CAN.write(heartbeat);
+        CAN.write(0x02);
+        CAN.write(0x03);
+        CAN.write(0x04);
+        CAN.write(0x05);
+        CAN.write(0x06);
+        CAN.write(0x07);
+        CAN.write(0x08);
+        CAN.endPacket();
+
+        // digitalWrite(LED_PIN, !digitalRead(LED_PIN));
     }
 }
